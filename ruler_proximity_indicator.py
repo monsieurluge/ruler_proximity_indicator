0d9e768b2674e8e362bb5e088547642ec0553c6a
monsieurluge <monsieurluge@gmail.com>
2021-02-02 22:01:06 +0100

    minor refactoring

--- ruler_proximity_indicator.py
+++ ruler_proximity_indicator.py
@@ -7,10 +7,17 @@ def closest(a, b):
     return a if abs(a) <= abs(b) else b
 
 
-def indicator(proximity):
+def draw_indicator(proximity):
     index  = min(6, max(0, proximity + 3))
     cursor = '╬' if index == 3 else '═'
     return replace_char('───┼───', cursor, index)
+
+
+def line_infos(view):
+    line_point  = view.sel()[0].begin()
+    line_length = view.line(line_point).size()
+    line_number = view.rowcol(line_point)[0]
+    return { 'length': line_length, 'number': line_number }
 
 
 def replace_char(text, char, index):
@@ -27,12 +34,10 @@ class RulerProximityIndicator(sublime_plugin.EventListener):
             self.rulers = settings.get('rulers')
 
     def on_selection_modified_async(self, view):
-        current_line_point  = view.sel()[0].begin()
-        line_length         = view.line(current_line_point).size()
-        current_line        = { 'length': line_length, 'number': view.rowcol(current_line_point)[0] }
-        proximity_to_rulers = map(lambda r: (line_length - r), self.rulers)
-        closest_one         = reduce(closest, proximity_to_rulers, 1000)
+        current_line = line_infos(view)
         if current_line == self.last_line:
             return
-        self.last_line = current_line
-        view.set_status('proximity_indicator', indicator(closest_one))
+        self.last_line      = current_line
+        proximity_to_rulers = map(lambda r: (current_line['length'] - r), self.rulers)
+        closest_one         = reduce(closest, proximity_to_rulers, 1000)
+        view.set_status('proximity_indicator', draw_indicator(closest_one))
