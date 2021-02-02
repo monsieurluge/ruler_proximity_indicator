59b03faa26c42d08cfdfa5cee54495cf75f1e42e
monsieurluge <monsieurluge@gmail.com>
2021-02-02 21:39:54 +0100

    do not update the status if the line does not change

--- ruler_proximity_indicator.py
+++ ruler_proximity_indicator.py
@@ -19,6 +19,7 @@ def replace_char(text, char, index):
 
 class RulerProximityIndicator(sublime_plugin.EventListener):
     rulers = [80]
+    last_line = { 'length': 0, 'number': 0 }
 
     def on_activated(self, view):
         settings = sublime.load_settings('Preferences.sublime-settings')
@@ -28,6 +29,10 @@ class RulerProximityIndicator(sublime_plugin.EventListener):
     def on_selection_modified_async(self, view):
         current_line_point  = view.sel()[0].begin()
         line_length         = view.line(current_line_point).size()
+        current_line        = { 'length': line_length, 'number': view.rowcol(current_line_point)[0] }
         proximity_to_rulers = map(lambda r: (line_length - r), self.rulers)
         closest_one         = reduce(closest, proximity_to_rulers, 1000)
+        if current_line == self.last_line:
+            return
+        self.last_line = current_line
         view.set_status('proximity_indicator', indicator(closest_one))
